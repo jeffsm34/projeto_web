edca8c73b6a47242879201205d115b1179d0812c
Jefferson Salustiano Marinho <jeff_sm34@protonmail.com>
2023-09-04 12:02:22 -0300

    Teste em uma linha de comentario

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 ﻿<?php
     // O Comando require insere o código de outro arquivo no atual.
     // O Comando include faz o mesmo processo, porém, não torna a inserção obrigatória.
+    // Teste de alteracao.
     require 'template/header.php';
 ?>
 
